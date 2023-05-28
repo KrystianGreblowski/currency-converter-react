61621af8c5ec4869bec13356c9783947a256af3f
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-28 13:16:38 +0200

    Form component added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import Form from "./Form";
+
 function App() {
   return (
     <div className="container">
@@ -5,35 +7,7 @@ function App() {
         <h1 className="header__title">Kalkulator waluty Euro</h1>
       </header>
 
-      <form className="form">
-        <fieldset className="form__fieldset">
-          <legend className="form__legend">Przelicznik EUR/PLN</legend>
-          <div className="form__body">
-            <span className="form__field">Kwota</span>
-            <div className="form__inputValue">
-              <input
-                className="form__field form__field--input"
-                type="number"
-                name="inputAmount"
-                placeholder="0.00"
-              />
-              <select
-                className="form__field form__field--select"
-                name="currency"
-              >
-                <option value="eur" selected>
-                  EUR
-                </option>
-                <option value="pln">PLN</option>
-              </select>
-            </div>
-            <span className="form__field">Kurs</span>
-            <span className="form__field"></span>
-            <span className="form__field">Wynik</span>
-            <span className="form__field form__field--result">0.00 PLN</span>
-          </div>
-        </fieldset>
-      </form>
+      <Form />
 
       <footer className="footer">Copyright © 2023 Krystian Gręblowski</footer>
     </div>
---
+++ src/Form/index.js
@@ -0,0 +1,32 @@
+import "./index.css";
+
+const Form = () => (
+  <form className="form">
+    <fieldset className="form__fieldset">
+      <legend className="form__legend">Przelicznik EUR/PLN</legend>
+      <div className="form__body">
+        <span className="form__field">Kwota</span>
+        <div className="form__inputValue">
+          <input
+            className="form__field form__field--input"
+            type="number"
+            name="inputAmount"
+            placeholder="0.00"
+          />
+          <select className="form__field form__field--select" name="currency">
+            <option value="eur" selected>
+              EUR
+            </option>
+            <option value="pln">PLN</option>
+          </select>
+        </div>
+        <span className="form__field">Kurs</span>
+        <span className="form__field"></span>
+        <span className="form__field">Wynik</span>
+        <span className="form__field form__field--result">0.00 PLN</span>
+      </div>
+    </fieldset>
+  </form>
+);
+
+export default Form;

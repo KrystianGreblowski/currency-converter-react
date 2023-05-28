030f9dd91e940c486b25649c683489cceecb9c9e
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-28 21:45:33 +0200

    code formatted

--- src/Form/ExchangeRate/index.js
+++ src/Form/ExchangeRate/index.js
@@ -1,3 +1,5 @@
-const exchangeRate = ({ eurRate, plnRate }) => <span className="form__field">1 PLN = {eurRate} EUR</span>;
+const exchangeRate = ({ eurRate, plnRate }) => (
+  <span className="form__field">1 PLN = {eurRate} EUR</span>
+);
 
 export default exchangeRate;

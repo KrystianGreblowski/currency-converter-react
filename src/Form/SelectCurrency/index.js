3baf8e5ff453894e2a3a3d3073318a5637e49149
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-28 17:28:45 +0200

    SelectCurrency component added

---
+++ src/Form/SelectCurrency/index.js
@@ -0,0 +1,10 @@
+const SelectCurrency = () => (
+  <select className="form__field form__field--select" name="currency">
+    <option value="eur" selected>
+      EUR
+    </option>
+    <option value="pln">PLN</option>
+  </select>
+);
+
+export default SelectCurrency;
--- src/Form/index.js
+++ src/Form/index.js
@@ -4,6 +4,7 @@ import Label from "./Label";
 import ExchangeRate from "./ExchangeRate";
 import Result from "./Result";
 import Input from "./Input";
+import SelectCurrency from "./SelectCurrency";
 
 const Form = () => (
   <form className="form">
@@ -12,15 +13,12 @@ const Form = () => (
 
       <div className="form__body">
         <Label title="Kwota" />
+
         <div className="form__inputValue">
           <Input />
-          <select className="form__field form__field--select" name="currency">
-            <option value="eur" selected>
-              EUR
-            </option>
-            <option value="pln">PLN</option>
-          </select>
+          <SelectCurrency />
         </div>
+
         <Label title="Kurs" />
         <ExchangeRate eurRate="4.8991" plnRate="0.2333" />
         <Label title="Wynik" />

836980e9565b2853f2063f604384772e546504a8
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-28 12:50:45 +0200

    app code init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header className="header">
+        <h1 className="header__title">Kalkulator waluty Euro</h1>
       </header>
+
+      <form className="form">
+        <fieldset className="form__fieldset">
+          <legend className="form__legend">Przelicznik EUR/PLN</legend>
+          <div className="form__body">
+            <span className="form__field">Kwota</span>
+            <div className="form__inputValue">
+              <label>
+                <input
+                  className="form__field form__field--input"
+                  type="number"
+                  name="inputAmount"
+                  placeholder="0.00"
+                />
+              </label>
+              <select
+                className="form__field form__field--select"
+                name="currency"
+              >
+                <option value="eur" selected>
+                  EUR
+                </option>
+                <option value="pln">PLN</option>
+              </select>
+            </div>
+            <span className="form__field">Kurs</span>
+            <span className="form__field"></span>
+            <span className="form__field">Wynik</span>
+            <span className="form__field form__field--result">0.00 PLN</span>
+          </div>
+        </fieldset>
+      </form>
+
+      <footer className="footer">Copyright © 2023 Krystian Gręblowski</footer>
     </div>
   );
 }

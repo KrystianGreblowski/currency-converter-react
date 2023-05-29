85526def6fd6bb4f4a91b4d169fcfed26c91ee6b
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-29 20:33:48 +0200

    example result value changed

--- src/Form/Result/index.js
+++ src/Form/Result/index.js
@@ -1,5 +1,5 @@
 const Result = () => (
-  <span className="form__field form__field--result">0.01 PLN</span>
+  <span className="form__field form__field--result">0.00 PLN</span>
 );
 
 export default Result;

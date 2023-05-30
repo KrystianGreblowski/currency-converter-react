a6011f2aca3fa27a20516cb94154effd9c68a0b3
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-05-30 22:39:04 +0200

    controlled component added

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,30 +1,44 @@
 import "./style.css";
+import { useState } from "react";
 
-const Form = () => (
-  <form className="form">
-    <fieldset className="form__fieldset">
-      <legend className="form__legend">Przelicznik EUR/PLN</legend>
+const Form = () => {
+  const [inputValue, setInputValue] = useState("");
 
-      <div className="form__body">
-        <span className="form__field">Kwota</span>
+  return (
+    <form className="form">
+      <fieldset className="form__fieldset">
+        <legend className="form__legend">Przelicznik EUR/PLN</legend>
 
-        <div className="form__inputValue">
-          <input className="form__field form__field--input" />
+        <div className="form__body">
+          <span className="form__field">Kwota</span>
 
-          <select className="form__field form__field--select">
-            <option value="eur">EUR</option>
-            <option value="pln">PLN</option>
-          </select>
+          <div className="form__inputValue">
+            <input
+              className="form__field form__field--input"
+              placeholder="0.00"
+              value={inputValue}
+              onChange={({target}) => setInputValue(target.value)}
+            />
+
+            <select className="form__field form__field--select">
+              <option value="eur">
+                EUR
+              </option>
+              <option value="pln">
+                PLN
+              </option>
+            </select>
+          </div>
+
+          <span className="form__field">Kurs</span>
+          <span className="form__field"></span>
+
+          <span className="form__field">Wynik</span>
+          <span className="form__field form__field--result">0.00 PLN</span>
         </div>
-
-        <span className="form__field">Kurs</span>
-        <span className="form__field"></span>
-
-        <span className="form__field">Wynik</span>
-        <span className="form__field form__field--result">0.00 PLN</span>
-      </div>
-    </fieldset>
-  </form>
-);
+      </fieldset>
+    </form>
+  );
+};
 
 export default Form;

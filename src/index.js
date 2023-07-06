67bc20a79b5c3537d0f08113d82c54439b5583bf
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-07-06 20:17:15 +0200

    globalStyle renamed to GlobalStyle

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
-import { GlobalStyle } from "./globalStyle";
+import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(

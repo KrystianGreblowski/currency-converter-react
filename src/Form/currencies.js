08d87abe581ffbf9300581bd38d894143b1c7994
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-07-16 16:45:37 +0200

    name and shortName added

--- src/Form/currencies.js
+++ src/Form/currencies.js
@@ -1,12 +1,14 @@
 export const currencies = [
   {
     id: 1,
-    name: "EUR",
+    name: "Euro",
+    shortName: "EUR",
     rate: 4.4356,
   },
   {
     id: 2,
-    name: "PLN",
+    name: "Polski Złoty",
+    shortName: "PLN",
     rate: 0.2209,
   },
 ];

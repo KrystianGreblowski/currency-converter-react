8477ad016b0b8a1da084cce4cbfcff1efe978ec5
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-07-29 15:34:47 +0200

    rates changed

--- src/Form/currencies.js
+++ src/Form/currencies.js
@@ -3,18 +3,18 @@ export const currencies = [
     id: 1,
     name: "Euro",
     shortName: "EUR",
-    rate: 4.4356,
+    rate: 0.2221,
   },
   {
     id: 2,
     name: "Dolar amerykański",
     shortName: "USD",
-    rate: 4.2232,
+    rate: 0.2344,
   },
   {
     id: 3,
     name: "Funt brytyjski",
     shortName: "GBP",
-    rate: 4.9954,
+    rate: 0.1999,
   },
 ];

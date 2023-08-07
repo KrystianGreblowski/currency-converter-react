80f46459c74376b9dfff4d824fba7aafb9f720c4
Krystian Greblowski <krystian.greblowski@gmail.com>
2023-08-07 21:55:05 +0200

    padding added

--- src/Form/CurrentDate/styled.js
+++ src/Form/CurrentDate/styled.js
@@ -5,9 +5,11 @@ export const StyledCurrentDate = styled.span`
   font-size: 14px;
   grid-column: span 2;
   justify-self: right;
+  padding: 0px;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     grid-column: span 1;
     justify-self: center;
+    padding: 5px;
   }
 `;
